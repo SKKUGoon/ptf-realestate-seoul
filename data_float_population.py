78ecaae4b115be6b6c8dd9a8ae1561c58fb590d2
SKKUGoon <baesangil0906@gmail.com>
2023-12-01 12:17:47 +0900

    change init. mount load_data function when the class is initiated

--- data_float_population.py
+++ data_float_population.py
@@ -5,9 +5,12 @@ from util import clean_remove_bracket, clean_subway_line_name
 
 
 class FloatPopulationSubwayTime:
-    def __init__(self):
+    def __init__(self, usage_month: int | None = None):
         # Population data
         self.data = self.load_data()
+        if usage_month is not None:
+            print(f"Using only {usage_month} data")
+            self.data = self.data.loc[self.data['사용월'] == usage_month]
 
     @staticmethod
     def load_data():
@@ -22,54 +25,7 @@ class FloatPopulationSubwayTime:
 
         return data
 
-    # def attach_location(self):
-    #     location = SubwayMapper()
-
-
-
-class FloatPopulationSubwayDay:
-    columns = ["date", "line", "station", "influx", "efflux", "regdate", "_del_"]
-
-    def __init__(self):
-        self.data = pd.DataFrame()
-        ...
-
-    def load_data(self, from_year: int = 2015, to_year: int = 2023):
-        loader = Loader(max_month=10)
-        loader.set_path("./asset/float_subway")
-
-        # Data separated by month
-        year_with_month = {2023}
-        for y in range(from_year, to_year + 1):
-            if y in year_with_month:
-                yearly_data = pd.DataFrame()
-
-                # Update monthly data
-                for m in range(1, 13):
-                    fn = loader.data_filename_subway_day(y, m)
-
-                    if fn == "":
-                        # Data's filename wasn't able to generate
-                        continue
-
-                    # Clean csv data
-                    monthly_data = pd.read_csv(fn).reset_index()
-                    monthly_data.columns = self.columns
-
-                    yearly_data = pd.concat([yearly_data, monthly_data], axis=0)
-            else:
-                fn = loader.data_filename_subway_day(y)
-                yearly_data = pd.read_csv(fn, encoding='euc-kr')
-
-            self.data = yearly_data
-
 
 if __name__ == "__main__":
-    # float_population = FloatPopulationSubwayDay()
-    # float_population.load_data(2023, 2023)
-    # pd.read_csv(ind)
-
-    float_population_time = FloatPopulationSubwayTime()
-    float_population_time.load_data()
-
-
+    fpt = FloatPopulationSubwayTime()
+    df = fpt.data
